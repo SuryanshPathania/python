b324e5af538682e7dd2a6de7af2471bc4fae1d70
Suryansh Pathania <sp.ssdps@gmail.com>
2024-05-05 21:53:59 +0530

    xc

--- first.py
+++ first.py
@@ -2,5 +2,32 @@ print("hello duniya")
 print("my name is Suraynsh", "my age is 22")
 print (23)
 print (23+2)
+# sigle line 
+a = 5
+b = 2
+print (a**b) #a^b
 
+sury = 10
+sury += 10 #add it in number
+print ("new number is",sury)
+
+val1 = True
+val2 = False
+print("and operator", val1 and val2)
+print("or operator", val1 or val2)
+# not ulta kardeta 
+
+# type conversion
+# a ="2" 
+a = int ("2")
+b = 3 
+print (a + b) # error we have to type cast 
+
+
+
+# name =input("enter you name:")
+# print ("Welcome", name)
+name = input("entr name: ")
+age = int(input("entr age: "))
+marks = int(input("entr marks: "))
 
